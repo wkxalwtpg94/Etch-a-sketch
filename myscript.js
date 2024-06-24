858a7e7773ead00c5b988e8eef7a9b94ec54b5a0
Ken <kenxhen@gmail.com>
2024-06-24 23:59:36 +0800

    Format 16x16 into square grid

--- myscript.js
+++ myscript.js
@@ -1,15 +1,29 @@
 // Create Container Grid
 const main = document.querySelector("body")
 const container = document.createElement("div");
+container.classList.add("container")
 main.appendChild(container)
 
 
 
 
 // Create 16x16 grid of square divs
-for (let index = 0; index < 16; index++) {
+for (let index = 0; index < 256; index++) {
     let current_element = document.createElement("div")
     current_element.classList.add("square_div")
     current_element.textContent = "Test Input"
     container.appendChild(current_element)
 }
+
+
+const all_squares = document.querySelectorAll(".square_div");
+
+all_squares.forEach((eachsquare) => {
+    eachsquare.addEventListener("mouseover", () => {
+
+        eachsquare.style.background-color = "blue";
+
+
+    });
+
+});
