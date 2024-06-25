7430059b71b51d90ae90c2c8291bb12e3f0d26e5
Ken <kenxhen@gmail.com>
2024-06-25 14:09:32 +0800

    Add function to create Grid & mouseover event

--- myscript.js
+++ myscript.js
@@ -6,24 +6,50 @@ main.appendChild(container)
 
 
 
+//Create Square Grid Function here
+function createGrid(n) {
+    //Create Grid
+    for (let index = 0; index < n*n; index++) {
+        let current_element = document.createElement("div")
+        current_element.classList.add("square_div")
+        
+        container.appendChild(current_element)
 
-// Create 16x16 grid of square divs
-for (let index = 0; index < 256; index++) {
-    let current_element = document.createElement("div")
-    current_element.classList.add("square_div")
-    current_element.textContent = "Test Input"
-    container.appendChild(current_element)
-}
-
-
-const all_squares = document.querySelectorAll(".square_div");
-
-all_squares.forEach((eachsquare) => {
-    eachsquare.addEventListener("mouseover", () => {
-
-        eachsquare.style.background-color = "blue";
+    }
 
 
+    // Add Mouseover Effect
+    const all_squares = document.querySelectorAll(".square_div");
+    all_squares.forEach((eachsquare) => {
+        eachsquare.style.width = `${99/n}%`
+        
+        
+        eachsquare.addEventListener("mouseover", () => {
+
+            eachsquare.style.backgroundColor = "blue";
+        });
     });
 
-});
+};
+
+
+
+// Event Listener for Button 
+const btn = document.querySelector("button")
+btn.addEventListener("click", () => {
+    //Get New Dimension
+    let squaresize = Number(prompt("How many squares for the square grid?"))
+    
+    // Remove Old Grid
+    const all_squares_one = Array.from(document.querySelectorAll(".square_div"))
+    for (let index = 0; index < all_squares_one.length; index++) {
+        container.removeChild(all_squares_one[index]);
+        
+    }
+
+    createGrid(squaresize)
+
+})
+
+
+
